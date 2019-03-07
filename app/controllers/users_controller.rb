24e20022777fb7966e959e42f8ed6c390234fba1
pop-k <44745055+pop-k@users.noreply.github.com>
2019-03-07 17:41:27 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,24 +3,24 @@ class UsersController < ApplicationController
   before_action :authenticate_user! ,except: [:update, :destroy]
 
   def index
-		  @users = User.all
-	end
+      @users = User.all
+  end
 
-	def show
-		  @user = User.find(params[:id])
-		  @contents = Content.where(user_id: @user.id)
-	end
+  def show
+      @user = User.find(params[:id])
+      @contents = Content.where(user_id: @user.id)
+  end
 
-	def following
+  def following
       @user  = User.find(params[:id])
       @users = @user.following
       redirect_to show_follow_path
   end
 
   def followers
-   	  @user  = User.find(params[:id])
-   	 	@users = @user.followers
-   	  redirect_to show_follow_path
+      @user  = User.find(params[:id])
+      @users = @user.followers
+      redirect_to show_follow_path
   end
 
   def show_following
@@ -38,8 +38,8 @@ class UsersController < ApplicationController
   end
 
   def payment_request
-  		@user = User.find(current_user[:id])
-  	  @payment = Payment.new
+      @user = User.find(current_user[:id])
+      @payment = Payment.new
       @payments = Payment.where(user_id: @user.id).page(params[:page]).per(20).order(id: "DESC")
   end
 
@@ -48,18 +48,18 @@ class UsersController < ApplicationController
       @nagesens = Nagesen.where(user_id: current_user.id)
   end
 
-	def edit
-		  @user = User.find(params[:id])
-      if current_user.id != @user.id
-         redirect_to user_path(current_user)
-      end
-	end
+  def edit
+      @user = User.find(params[:id])
+    if current_user.id != @user.id
+      redirect_to user_path(current_user)
+    end
+  end
 
-	def update
-		  @user = User.find(params[:id])
-		  @user.update(user_params)
-		  redirect_to user_path(@user.id)
-	end
+  def update
+      @user = User.find(params[:id])
+      @user.update(user_params)
+      redirect_to user_path(@user.id)
+  end
 
   def destroy
       @user = User.find(params[:id])
@@ -72,17 +72,16 @@ class UsersController < ApplicationController
   end
 
 private
-
-	def user_params
-    	params.require(:user).permit(:name, :image, :introduction)
- 	end
+  def user_params
+      params.require(:user).permit(:name, :image, :introduction)
+  end
 
   def balance_params
       params.require(:user).permit(:balance)
   end
 
- 	def payment_params
+  def payment_params
       params.require(:payment).permit(:payment)
- 	end
+  end
 end
 
