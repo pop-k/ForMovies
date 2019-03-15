ffb6b5dd88ddaab40d7fa5c9788d8295fc318ebf
pop-k <44745055+pop-k@users.noreply.github.com>
2019-03-15 12:41:38 +0900

    インデント修正

--- app/controllers/favorite_contents_controller.rb
+++ app/controllers/favorite_contents_controller.rb
@@ -1,24 +1,24 @@
 class FavoriteContentsController < ApplicationController
 
-  def create
-      @user_id = current_user.id
-      @content_id = Content.find(params[:id]).id
-      @favorite_content = FavoriteContent.new(content_id: @content_id, user_id: @user_id)
-     if @favorite_content.save
-        redirect_to favorite_contents_path
-     end
-  end
+        def create
+            @user_id = current_user.id
+            @content_id = Content.find(params[:id]).id
+            @favorite_content = FavoriteContent.new(content_id: @content_id, user_id: @user_id)
+         if @favorite_content.save
+            redirect_to favorite_contents_path
+         end
+        end
 
 
-  def destroy
-  	@favorite_content = FavoriteContent.find(params[:id])
-    if @favorite_content.destroy
-       redirect_to favorite_contents_path
-    end
-  end
+        def destroy
+  	        @favorite_content = FavoriteContent.find(params[:id])
+         if @favorite_content.destroy
+            redirect_to favorite_contents_path
+         end
+        end
 
-private
-      def content_params
-        params.require(:content).permit(:id, :content_id, :user_id)
-      end
+  private
+        def content_params
+            params.require(:content).permit(:id, :content_id, :user_id)
+        end
 end
