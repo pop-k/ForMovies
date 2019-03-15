3474f6eaaa05b41e5c102c8d9ed7a3b414c12231
pop-k <44745055+pop-k@users.noreply.github.com>
2019-03-15 12:36:09 +0900

    インデント修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,21 @@
 class CommentsController < ApplicationController
 
-  def create
-  	  # @content = Content.find(params[:content_id])
-  	  # (content_id: params[:content_id], user_id: current_user.id)
-      @comment = Comment.new(comment_params)
-      @comment.user_id = current_user.id
-      @content_path = @comment.content
-      @comment.save
-      redirect_to content_path(@content_path)
-  end
+	def create
+	    @comment = Comment.new(comment_params)
+	    @comment.user_id = current_user.id
+	    @content_path = @comment.content
+            @comment.save
+            redirect_to content_path(@content_path)
+        end
 
-  def destroy
-      @content = Content.find(params[:id])
-      @comment.destroy
-      redirect_to content_path(@content)
-  end
-private
+        def destroy
+            @content = Content.find(params[:id])
+            @comment.destroy
+            redirect_to content_path(@content)
+        end
+  private
 	def comment_params
-    	params.require(:comment).permit(:content_id, :user_id, :body)
+    	    params.require(:comment).permit(:content_id, :user_id, :body)
  	end
 
 end
